2282752cf83e0c48f53889cda1b8b3de5ec9dcde
Devanshi212 <dishi20021996@gmail.com>
2022-02-13 22:24:21 +0530

    data retrieval code and csv files

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,4 +1,15 @@
-
-# for i in range(len(companyList)):
-#     companyDict.update({companyList[i] : companyLinks[i]})
-# # print(companyDict)
+try:
+    #     #leverage Ratios button
+    #     wd.find_element(By .XPATH,'//*[@id="ratios"]/div[1]/ul/li[5]/a').click()
+    #     sleep(2)
+    #     # icr data for the past 4 years 
+    #     for i in range(2,6):
+    #         icrList.append(wd.find_element(By .XPATH,f'//*[@id="table_C_leverage_ratios_Ratios"]/tbody/tr[2]/td[{i}]').text)
+    # except:
+    #     #standalone leverage Ratios button
+    #     wd.find_element(By .XPATH,'//*[@id="stand_ratios"]/div[1]/ul/li[5]/a').click()
+    #     sleep(2)
+    #     # standalone icr data for the past 4 years 
+    #     for i in range(2,6):
+    #         icrList.append(wd.find_element(By .XPATH,f'//*[@id="table_S_leverage_ratios_Ratios"]/tbody/tr[2]/td[{i}]').text)
+    # # print(icrList) 
